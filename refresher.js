ba18830a4aac07df2a29b062567677e607566a69
Veronika Jones <veronikashapiro@yahoo.com>
2022-10-13 16:23:13 -0400

    adding content

---
+++ refresher.js
@@ -0,0 +1,16 @@
+let firstName = "Veronika";
+let lastName = "Jones";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let population = 3521000;
+console.log(population);
+let populationx3 = population * 3;
+console.log(populationx3);
+let javaScriptIsCool = true;
+let colors = ["red" + "orange" + "yellow" + "green" + "blue" + "purple"];
+for (let i = 1; i <= 5; i++) {
+console.log (i);
+}
+for (let j = 0; j <= 4; j++) {
+console.log (j);
+}
